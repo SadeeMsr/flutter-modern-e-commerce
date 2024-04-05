69b2c4f144bb7b6f4f3b823d05656893e3985dc8
Shad Ashfaque <sorin.shad@gmail.com>
2024-04-05 22:08:11 +0600

    assets committed

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -8,15 +8,12 @@ import '../utils/helpers/network_manager.dart';
 class GeneralBindings extends Bindings {
   @override
   void dependencies() {
-    /// -- Core
     Get.put(NetworkManager());
 
-    /// -- Product
     Get.put(CheckoutController());
     Get.put(VariationController());
     Get.put(ImagesController());
 
-    /// -- Other
     Get.put(AddressController());
   }
 }
