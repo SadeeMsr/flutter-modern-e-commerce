ed0a915ab39ffb8055d030d5bc144c824b360179
SadeeMsr <srrohan333@gmail.com>
2024-04-06 22:24:09 +0600

    Project folder structure

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:e_commerce/utils/theme/theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      
+    );
+  }
+}
